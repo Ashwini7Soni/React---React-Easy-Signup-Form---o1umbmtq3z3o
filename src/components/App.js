baf4902f66e93d7b83da4efd0a147f3a9ec41e99
Ashwini7Soni <soniashwini259@gmail.com>
2022-02-23 15:49:08 +0530

    fifth

--- src/components/App.js
+++ src/components/App.js
@@ -13,42 +13,44 @@ const App = () => {
 
   return (
     <div>
-      <form></form>
-      <div className = "signup-form" style={{display: "flex", flexDirection: "column", alignItems: "center"}} >
-        <h1>Signup Form</h1>
-        <div>
-          <label>Name : </label>
-          <input id = "name" value={name} placeholder = "name" onChange = {(event) => {
-            setName(event.target.value);
-          }} style = {{marginBottom: "0.8rem"}}/>
-          <label>{(error != null)? error.name : ""}</label>
+      <form>
+        <div className = "signup-form" style={{display: "flex", flexDirection: "column", alignItems: "center"}} >
+          <h1>Signup Form</h1>
+          <div>
+            <label>Name : </label>
+            <input id = "name" value={name} placeholder = "name" onChange = {(event) => {
+              setName(event.target.value);
+            }} style = {{marginBottom: "0.8rem"}}/>
+            <label>{(error != null)? error.name : ""}</label>
+          </div>
+          <div>
+            <label>Email : </label>
+            <input id = "email" value = {email} placeholder = "email" onChange = {(event) => {
+              setEmail(event.target.value);
+            }} style = {{marginBottom: "0.8rem"}}/>
+            <label>{(error != null)? error.email : ""}</label>
+          </div>
+          <div>
+            <label>Password : </label>
+            <input id = "password" value={password} placeholder = "password" onChange={(event) => {
+              setPassword(event.target.value);
+            }} style = {{marginBottom: "0.8rem"}}/>
+            <label>{(error != null)? error.password : ""}</label>
+          </div>
+          <div>
+            <label>Consent : </label>
+            <input type="checkbox" id = "consent" value = {consent} onChange={() => {
+              setConsent(prev => !prev);
+            }} style = {{marginBottom: "0.8rem"}}/>
+          </div>
+          <button type = "submit" onClick= {(e) => {
+            e.preventDefault();
+            const errObj = signUpFormValidation({name: name, email: email, password: password});
+            setError(errObj);
+            console.log(errObj);
+        }}>Signup</button>
         </div>
-        <div>
-          <label>Email : </label>
-          <input id = "email" value = {email} placeholder = "email" onChange = {(event) => {
-            setEmail(event.target.value);
-          }} style = {{marginBottom: "0.8rem"}}/>
-          <label>{(error != null)? error.email : ""}</label>
-        </div>
-        <div>
-          <label>Password : </label>
-          <input id = "password" value={password} placeholder = "password" onChange={(event) => {
-            setPassword(event.target.value);
-          }} style = {{marginBottom: "0.8rem"}}/>
-          <label>{(error != null)? error.password : ""}</label>
-        </div>
-        <div>
-          <label>Consent : </label>
-          <input type="checkbox" id = "consent" value = {consent} onChange={() => {
-            setConsent(prev => !prev);
-          }} style = {{marginBottom: "0.8rem"}}/>
-        </div>
-        <button type = "submit" onClick= {() => {
-          const errObj = signUpFormValidation({name: name, email: email, password: password});
-          setError(errObj);
-          console.log(errObj);
-      }}>Signup</button>
-      </div>
+      </form>
     </div>
   );
 }
